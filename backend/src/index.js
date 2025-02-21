0fb2bcadf832805341f4ffa704fb41172ac4850d
PatelParth2506 <patelparth53871@gmail.com>
2025-02-21 12:27:35 +0530

    cloudinary and mongodb setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import dotenv from 'dotenv'
 import { app } from "./app.js"
+import dbconnect from './db/dbconnect.js'
 
 dotenv.config({
     path:"../.env"
 })
 
+
+dbconnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("App Is Running On POrt",process.env.PORT)
+    })
+}).catch((err)=>{
+    console.log("Something Went Wrong",err)
+})
 
---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDIARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+const fileuploder=async(path)=>{
+    try {
+        if(!path) return null
+        const response=await cloudinary.uploader.upload(path,{
+            resource_type:auto
+        })
+        console.log(response)
+        return response
+    } catch (error) {
+        fs.unlinkSync(path)
+    }
+}
+
+export { fileuploder }
