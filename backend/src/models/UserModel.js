e9ddfe3ddb9ff75681ecff5af3af9466272225f0
PatelParth2506 <patelparth53871@gmail.com>
2025-02-25 22:46:03 +0530

    adding schema methods

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import ApiError from '../utils/ApiError.js'
+import jwt from 'jsonwebtoken'
 
 const userSchema=new mongoose.Schema({
     username:{
@@ -38,5 +41,41 @@ const userSchema=new mongoose.Schema({
 },{
     timestamps:true
 })
+
+userSchema.pre("save",async function(next) {
+    if(this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password) {
+    if(!password === !this.password){
+        return new ApiError(401,"Password Is Missing")
+    }
+
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.genrateAccessToken=async function(){
+    return jwt.sign({
+        _id:this._id,
+        username:this.username,
+        email:this.email
+    },process.env.ACCESS_TOKEN_SECRET,
+    {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.genrateRefreshToken=async function(){
+    return jwt.sign({
+        _id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const User=mongoose.model("User",userSchema)
