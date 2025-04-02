1636fa3cf57a531caf4b23c26cb1b588156380cb
1912-dishant <dishantitaliya619@gmail.com>
2025-04-02 17:27:23 +0530

    changes

--- newReactjs/src/components/Errorno5.jsx
+++ newReactjs/src/components/Errorno5.jsx
@@ -4,7 +4,7 @@ const Errorno5 = () => {
   return (
     <div className='w-screen flex justify-center items-center mt-12'>
     <div className='w-[400px] h-auto border-[2px] border-red-600 text-red-600 rounded-md shadow-md py-4 px-3 bg-white'>
-       <p className='text-center'>Please enter Username and Password correctly.how are you!!</p>
+       <p className='text-center'>Please enter Username and Password correctly. see it correctly</p>
     </div>
     </div>
   )
