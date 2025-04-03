c70cb1c6890bbc99b74af03c5e8e9fe6cead57ea
zeel-vaghasiya <zeelvaghasiya286@gmail.com>
2025-04-03 16:56:30 +0530

    save

--- newReactjs/src/App.jsx
+++ newReactjs/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Signup from "./components/signup"
-import { BrowserRouter,Routes, Route, useLocation } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import Home from "./components/Home"
 import Login from "./components/Login"
 import ChatLeft from "./components/ChatLeft"
@@ -21,46 +21,46 @@ import HomeLayout from "./components/HomeLayout.jsx"
 import NavBarOfWeb from "./components/NavBarOfWeb.jsx"
 import ProfileLayout from "./components/ProfileLayout.jsx"
 
-const Layout = ({ children }) =>{
-    const location = useLocation()
+const Layout = ({ children }) => {
+  const location = useLocation()
 
-    const showNavbar = location.pathname.startsWith("/store")
+  const showNavbar = location.pathname.startsWith("/store")
 
-    return(
-      <>
-        {showNavbar && <NavBarOfWeb />}
-        {children}
-      </>
-    )
+  return (
+    <>
+      {showNavbar && <NavBarOfWeb />}
+      {children}
+    </>
+  )
 }
 
 const App = () => {
-  return(
+  return (
     <Provider store={store}>
-   <BrowserRouter>
-      <Layout>
-      <Routes>
-          <Route path="/"  element={<Signup />} />
-          <Route path="/login"  element={<Login />} />
-          <Route path="/home"  element={<HomeLayout />} />
-          <Route path="/chatleftpart"  element={<ChatLeft />} />
-          <Route path="/chatrightempty"  element={<Chatrightempty />} />
-          <Route path="/chatbox"  element={<Chatbox />} />
-          <Route path="/chatrightpart"  element={<ChatRight />} />
-          <Route path="/editprofile"  element={<Editprofile />} />
-          <Route path="/profilelayout"  element={<ProfileLayout />} />
-          <Route path="/photo"  element={<Photo />} />
-          <Route path="/loader"  element={<Loader />} />
-          <Route path="/store" element={<StoreHome />} />
-          <Route path="/store/product/:id" element={<ProductDetails />} />
-          <Route path="/store/cart" element={<CartPage />} />
-          <Route path="/success" element={<PaymentSuccess/>}/>
-          <Route path="/success" element={<PaymentCancel/>}/>
-       </Routes>
-      </Layout>
-       
-   </BrowserRouter>
-  </Provider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<HomeLayout />} />
+            <Route path="/chatleftpart" element={<ChatLeft />} />
+            <Route path="/chatrightempty" element={<Chatrightempty />} />
+            <Route path="/chatbox" element={<Chatbox />} />
+            <Route path="/chatrightpart" element={<ChatRight />} />
+            <Route path="/editprofile" element={<Editprofile />} />
+            <Route path="/profilelayout" element={<ProfileLayout />} />
+            <Route path="/photo" element={<Photo />} />
+            <Route path="/loader" element={<Loader />} />
+            <Route path="/store" element={<StoreHome />} />
+            <Route path="/store/product/:id" element={<ProductDetails />} />
+            <Route path="/store/cart" element={<CartPage />} />
+            <Route path="/success" element={<PaymentSuccess />} />
+            <Route path="/success" element={<PaymentCancel />} />
+          </Routes>
+        </Layout>
+
+      </BrowserRouter>
+    </Provider>
   );
 }
 
