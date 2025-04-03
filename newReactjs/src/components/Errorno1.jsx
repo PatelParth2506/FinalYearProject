33fb7ea59cb44f52a21afb9b55cd61d2d4ee0a2f
zeel-vaghasiya <zeelvaghasiya286@gmail.com>
2025-04-03 13:22:55 +0530

    save

--- newReactjs/src/components/Errorno1.jsx
+++ newReactjs/src/components/Errorno1.jsx
@@ -4,7 +4,7 @@ const Errorno1 = () => {
   return (
     <div className='fixed top-5 left-1/2 transform -translate-x-1/2 w-full max-w-md px-4 z-50'>
     <div className='w-full border-2 border-red-600 bg-white text-red-600 rounded-md shadow-md py-3 px-6'>
-       <p className="text-center text-sm sm:text-base"> Password should be strong and contain alphabets, digits, underscores and special characters..</p>
+       <p className="text-center text-sm sm:text-base">Password should be strong and contain alphabets, digits, underscores and special characters..</p>
     </div>
     </div>
   )
