2f4cbfd0ca39721c05686b2ad90310cda6e87723
1912-dishant <dishantitaliya619@gmail.com>
2025-04-22 18:13:57 +0530

    save

--- newReactjs/src/components/PaymentSuccess.jsx
+++ newReactjs/src/components/PaymentSuccess.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import "./PaymentSuccess.css"; // Make sure to include the CSS file
 
 const PaymentSuccess = () => {
   const [animate, setAnimate] = useState(false);
@@ -13,7 +12,6 @@ const PaymentSuccess = () => {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#f0f4ff] via-[#fdf7f7] to-[#e1f0ff] px-4">
       <div className="w-full md:max-w-sm max-w-xs bg-white/40 backdrop-blur-2xl border border-white/30 rounded-2xl shadow-xl p-6 md:p-8 text-center flex flex-col items-center">
         
-        {/* ✅ Checkmark Icon with Sparkles Animation */}
         <div className="relative md:w-24 md:h-24 w-20 h-20 rounded-full bg-green-500 flex items-center justify-center text-white shadow-xl bounce-in">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -52,7 +50,7 @@ const PaymentSuccess = () => {
 
         <a
           href="/store"
-          className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-3 px-6 rounded-xl transition-all duration-300 shadow-md w-full sm:w-auto"
+          className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-6 rounded-xl transition-all duration-300 shadow-md w-full"
         >
           Back to Store
         </a>
